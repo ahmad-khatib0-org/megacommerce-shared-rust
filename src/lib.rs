474438291cca8fab9e08c05756359a726e40a9a6
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-09-06 15:38:36 +0300

    0.2.5

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ pub mod models;
 pub mod utils;
 
 #[cfg(any(feature = "macros", feature = "all"))]
-pub use megacommerce_shared_sanitize_derive::SanitizeAppError;
+pub use megacommerce_shared_sanitize_derive::sanitize_app_error;
