8fa3e38340d1961cfa5ada70a6db9e196a3724bd
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-09-18 14:50:51 +0300

    0.3.9

--- src/models/r_lock.rs
+++ src/models/r_lock.rs
@@ -3,9 +3,9 @@ use std::sync::Arc;
 use tokio::sync::{RwLock, RwLockReadGuard};
 
 #[derive(Debug, Clone)]
-pub struct RLock<T>(pub Arc<RwLock<T>>);
+pub struct RLock<T: ?Sized>(pub Arc<RwLock<T>>);
 
-impl<T> RLock<T> {
+impl<T: ?Sized> RLock<T> {
   pub async fn get(&self) -> RwLockReadGuard<'_, T> {
     self.0.read().await
   }
