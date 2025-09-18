3bb12fc7a0e673497c37e5d89d735301d9e74f90
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-09-18 18:48:53 +0300

    0.4.0

--- src/models/network.rs
+++ src/models/network.rs
@@ -1,21 +1,40 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use derive_more::Display;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
 pub enum Header {
+  #[display("authorization")]
   Authorization,
+  #[display("x-request-id")]
   XRequestId,
+  #[display("x-ip-address")]
   XIpAddress,
+  #[display("x-forwarded-for")]
   XForwardedFor,
+  #[display("path")]
   Path,
+  #[display("user-agent")]
   UserAgent,
+  #[display("accept-language")]
   AcceptLanguage,
+  #[display("session-id")]
   SessionId,
+  #[display("token")]
   Token,
+  #[display("created-at")]
   CreatedAt,
+  #[display("expires-at")]
   ExpiresAt,
+  #[display("last-activity-at")]
   LastActivityAt,
+  #[display("user-id")]
   UserId,
+  #[display("device-id")]
   DeviceId,
+  #[display("roles")]
   Roles,
+  #[display("is-oauth")]
   IsOauth,
+  #[display("props")]
   Props,
 }
 
