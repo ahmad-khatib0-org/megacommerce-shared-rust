299f2f09106dfd44a45235735d972f2f15aa36d4
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-11-13 13:14:33 +0300

    0.4.76

--- src/utils/time.rs
+++ src/utils/time.rs
@@ -1,4 +1,4 @@
-use chrono::Utc;
+use chrono::{NaiveDate, Utc};
 
 pub fn time_get_millis() -> u64 {
   Utc::now().timestamp_millis().try_into().unwrap()
@@ -6,4 +6,10 @@ pub fn time_get_millis() -> u64 {
 
 pub fn time_get_seconds() -> u64 {
   Utc::now().timestamp().try_into().unwrap()
+}
+
+pub fn date_to_milliseconds(date_str: &str) -> Result<String, Box<dyn std::error::Error>> {
+  let naive_date = NaiveDate::parse_from_str(date_str, "%Y-%m-%d")?;
+  let naive_datetime = naive_date.and_hms_opt(0, 0, 0).ok_or("Invalid time")?;
+  Ok(naive_datetime.and_utc().timestamp_millis().to_string())
 }
